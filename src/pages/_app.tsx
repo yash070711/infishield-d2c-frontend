63415c151cd7806e6015c4a4094a48a390a79e3a
infinitymasters2023 <techmasters@infinityassurance.com>
2024-03-05 15:10:57 +0530

    added page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { APP_INFO } from "@/environments";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>{APP_INFO.TITLE}</title>
+        <meta name="description" content={APP_INFO.DESCRIPTION} />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
