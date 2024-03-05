b94dfca63c14134a5a936bb9e5e2ebc5e996d20c
infinitymasters2023 <144238563+infinitymasters2023@users.noreply.github.com>
2024-03-05 16:21:55 +0530

    header changes

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,77 @@
-import styles from './styles/Header.module.scss';
+import  style from '../../styles/main.css'
 
 const Header: React.FC = () => {
     return (
         <>
-        <h1>Header Works</h1>
+      <header id="headerSection">
+            <div className="container-fluid">
+                <div className="row">
+                    {/* Top Header */}
+                    <div className="topHeader">
+                        <div className="container">
+                            <div className="row">
+                                <div className="navTop">
+                                    <ul>
+                                        <li className="dropdown">
+                                            <a className="dropdown-toggle" href="#" role="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false"> About</a>
+                                            <div className="dropdown-menu pb-0">
+                                                <div className="row">
+                                                    <div className="col-12">
+                                                        <a className="dropdown-item" href="about-InfyShield.html">InfyShield</a>
+                                                        <hr className="dropdown-divider w-100" />
+                                                        <a className="dropdown-item" href="board-members.html">Board Of Directors</a>
+                                                        <hr className="dropdown-divider w-100" />
+                                                        {/* Add more dropdown items */}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* Add more top header items */}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Main Header */}
+                    <div className="mainHeader">
+                        <div className="container p-0">
+                            <div className="middleHeader">
+                                <div className="middleHeader___Left">
+                                    <div className="headerbrand">
+                                        <a href="index.html"> <img src="assets/img/logo/logo.png" width="90px" height="98.6px"
+                                                alt="infyShield Logo" /></a>
+                                    </div>
+                                </div>
+                                {/* Add more middle header content */}
+                            </div>
+                        </div>
+
+                        {/* Bottom Header */}
+                        <div className="row">
+                            <div className="bottomHeader">
+                                <div className="container p-0">
+                                    <nav className="navbar navbar-expand-lg">
+                                        <div className="container-fluid p-0">
+                                            <div className="collapse navbar-collapse justify-content-end"
+                                                id="navbarNavDropdown">
+                                                <ul className="navbar-nav">
+                                                    <li className="nav-item">
+                                                        <a className="nav-link active" aria-current="page" href="index.html">Home</a>
+                                                    </li>
+                                                    {/* Add more bottom header items */}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
         </>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { APP_INFO } from "@/environments";
 import "@/styles/globals.css";
+import "@/styles/Header.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
